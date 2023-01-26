61f9869356c7890c47857d73bc76158b2333b67f
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-26 17:37:47 +0200

    Basic data types part

---
+++ JavaCourse/src/DataTypesAndVariables.java
@@ -0,0 +1,15 @@
+import java.awt.desktop.SystemSleepEvent;
+
+public class DataTypesAndVariables {
+    public static void main(String[] args) {
+        int age = 17;
+        double num2 = 5.0;
+        boolean b = true;
+        char c = 'a';
+        String name = "Ivaylo";
+
+        int num3 = age;
+
+        System.out.println(num3);
+    }
+}
