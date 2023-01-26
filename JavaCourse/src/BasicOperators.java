3883f9b4a6e3e58e51faf06e2a57b3c75348344f
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-26 17:52:45 +0200

    Basic operators part

---
+++ JavaCourse/src/BasicOperators.java
@@ -0,0 +1,12 @@
+public class BasicOperators {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 3;
+        int z = 56;
+
+        double u = z / (double)y;
+        double d = Math.pow(x, y);
+        System.out.println(u);
+
+    }
+}
