165ce9a1c91cec542203bbbce504e6b3fecb0898
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-27 12:48:39 +0200

    Arrays part

---
+++ JavaCourse/src/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] newArr = new String[5];
+        newArr[0] = "hello";
+        newArr[1] = "hi";
+        newArr[2] = "Ivaylo";
+        newArr[3] = "Emin";
+        newArr[4] = "Nakov";
+
+        int[] nums = {2, 3, 53, 12, 51};
+        double[] nums2 = {2.0, 5.0, 2.2};
+
+        double x = nums2[2];
+        System.out.println(x);
+    }
+}
