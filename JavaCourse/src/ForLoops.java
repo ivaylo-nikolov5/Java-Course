34a507f7b5f3cac9c9fce3390da1816b89457123
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-27 13:08:47 +0200

    For loops part

---
+++ JavaCourse/src/ForLoops.java
@@ -0,0 +1,12 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
