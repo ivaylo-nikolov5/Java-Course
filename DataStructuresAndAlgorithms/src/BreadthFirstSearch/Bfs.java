ab93f62b4b61c3e3e8cb0f5567b1715d5139ba45
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-07 16:53:53 +0200

    Breadth First Search(BFS) part

---
+++ DataStructuresAndAlgorithms/src/BreadthFirstSearch/Bfs.java
@@ -0,0 +1,22 @@
+package BreadthFirstSearch;
+
+public class Bfs {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.breadthFirstSearch(0);
+    }
+}
